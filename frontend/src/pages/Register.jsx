4fbfce604082d9ec08d62acd6189b2693d0b69ab
yasharyasaxena <105877652+yasharyasaxena@users.noreply.github.com>
2025-02-01 13:44:50 +0530

    register_page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,62 @@
+import { NavLink } from "react-router-dom";
+
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="flex w-fit py-20 px-10 mx-auto h-fit flex-col items-center flex-shrink-0 border rounded-3xl">
+      <div className="flex flex-col items-start gap-3">
+        <div className="flex flex-col items-start">
+          <h1 className="font-semibold text-4xl text-black">Register</h1>
+          <p className="text-black">Create an account to continue</p>
+        </div>
+        <form className="space-y-4">
+          <div className="flex w-80 py-3 px-4 items-center gap-2 rounded-xl border border-black">
+            <div className="flex flex-col items-start gap-6">
+              <input
+                type="text"
+                className="text-xl font-normal border-none focus:outline-none"
+                placeholder="Username"
+              />
+            </div>
+          </div>
+          <div className="flex w-80 py-3 px-4 items-center gap-2 rounded-xl border border-black">
+            <div className="flex flex-col items-start gap-6">
+              <input
+                type="email"
+                className="text-xl font-normal border-none focus:outline-none"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="flex w-80 py-3 px-4 items-center gap-2 rounded-xl border border-black">
+            <div className="flex flex-col items-start gap-6">
+              <input
+                type="password"
+                className="text-xl font-normal border-none focus:outline-none"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="flex w-80 py-3 px-4 items-center gap-2 rounded-xl border border-black">
+            <div className="flex flex-col items-start gap-6">
+              <input
+                type="password"
+                className="text-xl font-normal border-none focus:outline-none"
+                placeholder="Confirm Password"
+              />
+            </div>
+          </div>
+          <button className="flex w-80 py-3 px-4 items-center justify-center gap-2 rounded-xl bg-custom-gradient text-white">
+            Register
+          </button>
+        </form>
+        <hr className="w-80 border-t border-black" />
+        <div className="flex mx-auto justify-center gap-1">
+          <p className="text-black">Already have an account?</p>
+          <NavLink to="/signIn" className="text-blue-500">
+            Sign In
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,20 @@ export default {
   ],
   theme: {
     extend: {
-      "height": {
+      width: {
+        "30": "30rem",
+      },
+      height: {
         '85vh': '85vh',
+        '49.75': '49.75rem',
       },
       colors: {
         "dark-purple": "#081A51",
         "light-white": 'rgba(255, 255, 255, 0.18)',
       },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(91deg, #2E4CEE 9.91%, #221EBF 53.29%, #040F75 91.56%)',
+      }
     },
   },
   plugins: [],
