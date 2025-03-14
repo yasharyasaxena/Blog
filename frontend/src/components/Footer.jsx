e74f74aab45710903daddde29aeb69a6b08dad1b
yasharyasaxena <105877652+yasharyasaxena@users.noreply.github.com>
2025-03-15 01:36:05 +0530

    footer fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="p-10 border-t bg-[rgb(27,27,27)] text-white">
+    <div className="p-10 sticky mt-auto border-t bg-[rgb(27,27,27)] text-white">
       <p className="text-sm text-white">
         © Copyright 2025 Yash IIT Indore. All rights reserved.
       </p>
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <Outlet />
       <Footer />
